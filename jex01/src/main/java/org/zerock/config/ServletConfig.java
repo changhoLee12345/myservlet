27ae03ea28d8c93b59c5bfcb3df1016c0573a487
you <you@email.com>
2024-03-15 15:52:21 +0900

    part 2 done.

--- jex01/src/main/java/org/zerock/config/ServletConfig.java
+++ jex01/src/main/java/org/zerock/config/ServletConfig.java
@@ -1,6 +1,11 @@
 package org.zerock.config;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,7 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = { "org.zerock.controller" })
+@ComponentScan(basePackages = { "org.zerock.controller", "org.zerock.exception" })
 public class ServletConfig implements WebMvcConfigurer {
 
 	@Override
@@ -18,6 +23,7 @@ public class ServletConfig implements WebMvcConfigurer {
 		bean.setViewClass(JstlView.class);
 		bean.setPrefix("/WEB-INF/views/");
 		bean.setSuffix(".jsp");
+
 		registry.viewResolver(bean);
 
 	}
@@ -25,6 +31,18 @@ public class ServletConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+		resolver.setMaxInMemorySize(1024 * 1024);
+		resolver.setUploadTempDir(new FileSystemResource("c:\\uupload\\tmp"));
+		resolver.setDefaultEncoding("utf-8");
+
+		return resolver;
 	}
 
 }
--- jex01/src/main/java/org/zerock/config/WebConfig.java
+++ jex01/src/main/java/org/zerock/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.zerock.config;
 
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -16,5 +18,10 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
 	}
 }
