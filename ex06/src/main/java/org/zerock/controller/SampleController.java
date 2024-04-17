0947c908ba548793a1decc7afb1fe8d2d4b73042
home <home@home.com>
2024-04-17 22:24:12 +0900

    done

--- ex06/src/main/java/org/zerock/controller/SampleController.java
+++ ex06/src/main/java/org/zerock/controller/SampleController.java
@@ -10,17 +10,17 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
-	
+
 	@GetMapping("/all")
 	public void doAll() {
 		log.info("do all can access ");
 	}
-	
+
 	@GetMapping("/member")
 	public void doMember() {
 		log.info("do member can access");
 	}
-	
+
 	@GetMapping("/admin")
 	public void doAdmin() {
 		log.info("do admin can access");
